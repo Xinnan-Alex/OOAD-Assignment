b95829a24357f6d17ac7f24ca11112ca9b2fc40a
xinnan01991 <91050730+xinnan01991@users.noreply.github.com>
2021-10-10 18:18:55 +0800

    added in edit property information interface

---
+++ src/adminEditPropertySceneController.java
@@ -0,0 +1,77 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class adminEditPropertySceneController implements Initializable{
+
+    Property propertyToBeEdited;
+
+    @FXML
+    ComboBox<String> propTypeComboBox,propHiddenStatusComboBox,propRentalStatusComboBox,propOwnerComboBox;
+
+    @FXML
+    TextField propNameTextField,propSizeTextField,propOwnerContactNumTextField,propRentalRateTextField;
+
+    @FXML
+    Button confirmButton,backButton;
+
+    @FXML
+    Label propertyID;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        propTypeComboBox.getItems().clear();
+        propTypeComboBox.getItems().addAll(Globals.propertyType);
+        propHiddenStatusComboBox.getItems().clear();
+        propHiddenStatusComboBox.getItems().addAll("true","false");
+        propRentalStatusComboBox.getItems().clear();
+        propRentalStatusComboBox.getItems().addAll("active", "not Active");
+        propOwnerComboBox.getItems().addAll(Globals.LogicModel.getListofPropOwnerName());
+
+    }
+
+    public void passPropertyToBeEdited(Property propertypassedin){
+        propertyToBeEdited = propertypassedin;
+        
+        propNameTextField.setText(propertyToBeEdited.getProjectName());
+        propSizeTextField.setText(Long.toString(propertyToBeEdited.getPropertySize()));
+        propOwnerContactNumTextField.setText(propertyToBeEdited.getContactNum());
+        propRentalRateTextField.setText(Long.toString(propertyToBeEdited.getRentalRate()));
+        propTypeComboBox.setValue(propertyToBeEdited.getPropertyType());
+        propHiddenStatusComboBox.setValue(Boolean.toString(propertyToBeEdited.getHiddenStatus()));
+        propertyID.setText(Long.toString(propertyToBeEdited.getPropertyID()));
+        
+        String rentalStatus;
+        if (propertyToBeEdited.getRentStatus()){
+            rentalStatus = "active";
+        }
+        else{
+            rentalStatus = "not Active";
+        }
+
+        propRentalStatusComboBox.setValue(rentalStatus);
+        
+        propOwnerComboBox.setValue(propertyToBeEdited.getPropertyOwner());
+    }
+
+    public void confirmButtonHandler(){
+
+    }
+
+    public void backButtonHandler(){
+        
+    }
+
+    public void propOwnerComboBoxHandler(ActionEvent e) {
+        propOwnerContactNumTextField.setText(Globals.LogicModel.getUserContactNum(Globals.LogicModel.getUsername(propOwnerComboBox.getValue())));
+    
+    }
+    
+}
--- src/adminPropertyListingController.java
+++ src/adminPropertyListingController.java
@@ -5,20 +5,18 @@ import java.io.IOException;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
 import javafx.util.Callback;
 import javafx.fxml.FXML;
-import javafx.beans.Observable;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView.TableViewSelectionModel;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.fxml.FXMLLoader;
@@ -43,13 +41,10 @@ public class adminPropertyListingController implements Initializable{
     TextField propertyNameSearch;
 
     @FXML
-    Button addPropertyButton,deletePropertyButton,backButton;
+    Button addPropertyButton,deletePropertyButton,editPropertyButton,backButton,clearSearchButton;
 
     @FXML
     ComboBox<String> propertyTypeFilter;
-
-    @FXML
-    ImageView clearSearchIcon;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -156,12 +151,28 @@ public class adminPropertyListingController implements Initializable{
         
     }
 
-    public void clearSearchIconHandler(){
+    public void clearSearchButtonHandler(){
         propertyTypeFilter.getSelectionModel().clearSelection();
     }
 
-    public void editPropertyButton(){
+    public void editPropertyButtonHandler() throws IOException{
+        ObservableList<Property> propertyToBeEdited = propTableView.getSelectionModel().getSelectedItems();
+
+        if (propertyToBeEdited.isEmpty()){
+            (new Alert(AlertType.ERROR,"Please select a property to edit")).show();
+        }
+        else{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/fxml/admin/adminEditPropertyScene.fxml"));
+            Parent parent = loader.load();
+
+            adminEditPropertySceneController controller = loader.getController();
+            controller.passPropertyToBeEdited(propertyToBeEdited.get(0));
+            
+            Stage stage = (Stage) editPropertyButton.getScene().getWindow();
+            stage.setScene(new Scene(parent));
+        }
         
+
     }
     
 }
